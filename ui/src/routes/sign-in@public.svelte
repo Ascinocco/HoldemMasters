bc01658a8925f68185e8cb829d3afe8b8bbe62ee
Anthony Scinocco <scinocco.a@gmail.com>
2022-04-18 17:48:14 -0400

    Basic sign in with browser validation

--- ui/src/routes/sign-in@public.svelte
+++ ui/src/routes/sign-in@public.svelte
@@ -1,36 +1,70 @@
 <script lang="ts">
+  type FormErrors = {
+    email?: string;
+    password?: string;
+  } | undefined;
+
   let email = "";
   let password = "";
+  let errors;
+
+  const validateFormData = () => {
+    let errors: FormErrors = undefined;
+
+    if (!email) {
+      errors = {
+        email: "Email is required"
+      }
+    }
+
+    if (!password) {
+      errors = {
+        ...errors,
+        password: "Password is required",
+      }
+    }
+
+    return errors;
+  };
 
   const handleSubmit = (e) => {
-    console.log("submit event", e);
+    errors = validateFormData();
+    if (errors) return;
   };
 </script>
 
 <div class="flex justify-center h-screen items-center">
   <div class="card w-96 bg-base-100 bg-slate-400 shadow-xl h-1/3 text-black nav-offset card-min-height">
-    <div class="card-body justify-between">
-      <div>
+    <div class="card-body">
+      <div class="justify-between">
         <h1 class="card-title pb-5">Sign In</h1>
         <form id="sign-in" on:submit|preventDefault={handleSubmit}>
           <div class="form-control w-full max-w-xs">
             <label for="email" class="label">
               <span class="label-text-alt text-black">Email</span>
             </label>
-            <input bind:value={email} name="email" type="text" placeholder="Email" class="input input-bordered w-full max-w-xs text-slate-400">
-            <span class="label-text-alt text-rose-800 p-1 input-error">Error</span>
+            <input bind:value={email} name="email" type="email" placeholder="Email" class="input input-bordered w-full max-w-xs text-slate-400">
+            <span class="label-text-alt text-rose-800 p-1 input-error">
+              {#if errors && errors.email}
+                {errors.email}
+              {/if}
+            </span>
           </div>
-          <div class="form-control w-full max-w-xs">
+          <div class="form-control w-full max-w-xs pb-8">
             <label for="password" class="label">
               <span class="label-text-alt text-black">Password</span>
             </label>
             <input bind:value={password} name="password" type="password" placeholder="Password" class="input input-bordered w-full max-w-xs text-slate-400">
-            <span class="label-text-alt text-rose-800 p-1 input-error">Error</span>
+            <span class="label-text-alt text-rose-800 p-1 input-error">
+              {#if errors && errors.password}
+                {errors.password}
+              {/if}
+            </span>
+          </div>
+          <div class="card-actions">
+            <button for="sign-in" class="btn btn-primary btn-block" type="submit">Sign In</button>
           </div>
         </form>
-      </div>
-      <div class="card-actions">
-        <button class="btn btn-primary btn-block">Sign In</button>
       </div>
     </div>
   </div>
