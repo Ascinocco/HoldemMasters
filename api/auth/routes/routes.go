125713c2df4d702b60da736084c44221e99c9ca2
Anthony Scinocco <scinocco.a@gmail.com>
2022-04-23 15:19:54 -0400

    Add todo for getting user

--- api/auth/routes/routes.go
+++ api/auth/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 const API_VERSION = "/api/v1"
 
+// @TODO: Add route to get user
 var PublicRoutes = map[string]string{
 	"CreateUser":    API_VERSION + "/user",
 	"CreateSession": API_VERSION + "/session",
