1dded4d1bed9470fe9570f6cdda579640dd2c301
Anthony Scinocco <scinocco.a@gmail.com>
2022-04-23 15:19:25 -0400

    Pull user data from store and display it.

--- ui/src/routes/app/play.svelte
+++ ui/src/routes/app/play.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+  import { user } from "../../stores/user";
+  const email = $user.email;
+  const username = $user.username; 
+</script>
+
 <div>
+  <h2>{email || "not set"}</h2>
+  <h2>{username || "not set"}</h2>
   Logged in
 </div>
